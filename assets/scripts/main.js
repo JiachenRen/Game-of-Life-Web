c863fca23f688d5c8e572806448c26ef3f6cc2d0
Jiachen Ren <jiachenren@greensboroday.org>
2017-07-21 09:03:00 -0400

    -renovated page design -algorithm speed up -bug fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,57 @@
 var cellMatrix;
+var resultMat;
 var recordedMatrix;
-var margin = 1;
-var cellWidth;
-var pixelDensity2 = true;
+var margin = 0;
+var cellWidth = 10;
+var pixelDensity2 = true; //set pixel density to 2.
 var setting;
 var iteratingThreadIds;
-var rows = 100;
-var cols = 100;
 
 //grab the canvas and context from HTML document.
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+$(document).ready(function () {
+    window.addEventListener("resize", renew);
+});
+
+//TODO: debug
+//prevents mobile devices from scrolling.
+$(document).on('touchstart', function (e) {
+    if (e.target.nodeName !== 'INPUT') {
+        e.preventDefault();
+        mouseDownEvtHandler(e);
+    }
+});
+
+//TODO: debug
+$(document).on('touchmove', mouseMoveEvtHandler);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    //updates the pixel density
+    if (pixelDensity2) {
+        canvas.style.width = canvas.width + "px";
+        canvas.style.height = canvas.height + "px";
+        canvas.width = canvas.width * 2;
+        canvas.height = canvas.height * 2;
+    }
+}
+
+function renew() {
+    clearAllThreads();
+    resizeCanvas();
+    window["cols"] = parseInt(canvas.width / cellWidth);
+    window["rows"] = parseInt(canvas.height / cellWidth);
+    cellWidth = (canvas.width / cols + canvas.height / rows) / 2;
+    initCellMatrix();
+    requestAnimationFrame(draw);
+}
 
 function init() {
-    //set pixel density to 2.
-    (function resizeCanvas() {
-        var smaller = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth;
-        canvas.width = smaller;
-        canvas.height = smaller;
-
-        //updates the pixel density
-        if (pixelDensity2) {
-            canvas.style.width = canvas.width + "px";
-            canvas.style.height = canvas.height + "px";
-            canvas.width = canvas.width * 2;
-            canvas.height = canvas.height * 2;
-        }
-        requestAnimationFrame(draw);
-    })();
+    renew();
 
     //init mouse event listeners
     canvas.addEventListener("mousedown", mouseDownEvtHandler, false);
@@ -36,15 +59,12 @@ function init() {
     window.addEventListener("mouseup", function () {
         setting = false
     }, false);
-    //initialize
-    initCellMatrix();
+
     requestAnimationFrame(draw);
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawMatrix(margin, margin, canvas.width - margin * 2, canvas.height - margin * 2);
-    requestAnimationFrame(draw);
+    updateCells(margin, margin);
 }
 
 function initCellMatrix() {
@@ -59,37 +79,19 @@ function initCellMatrix() {
     }
     console.log("actual: " + cellMatrix.length + " " + cellMatrix[0].length);
     recordedMatrix = cellMatrix;
+    resultMat = cellMatrix.map(function (arr) {
+        return arr.slice();
+    });
 }
 
-function drawMatrix(x, y, w, h) {
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 1;
-    var widthFromW = w / cellMatrix[0].length;
-    var widthFromH = h / cellMatrix.length;
-    cellWidth = widthFromH > widthFromW ? widthFromW : widthFromH;
-
-    ctx.beginPath();
-    //draws the horizontal line
-    for (var r = 1; r < cellMatrix.length; r++) {
-        ctx.moveTo(x, y + r * cellWidth + 0.5);
-        ctx.lineTo(x + w, y + r * cellWidth + 0.5);
-    }
-    //draws the vertical line
-    for (var c = 1; c < cellMatrix[0].length; c++) {
-        ctx.moveTo(x + c * cellWidth + 0.5, y);
-        ctx.lineTo(x + c * cellWidth + 0.5, y + h);
-    }
-    ctx.stroke();
-
-    //draws the bonding box
-    ctx.strokeRect(x - 0.5, y - 0.5, w, h);
-
-    //draws the active cells
-    ctx.fillStyle = "black";
-    for (r = 0; r < cellMatrix.length; r++) {
-        for (c = 0; c < cellMatrix[0].length; c++) {
-            if (cellMatrix[r][c]) {
-                ctx.fillRect(x + c * cellWidth, y + r * cellWidth, cellWidth, cellWidth);
+function updateCells(x, y) {
+    for (var r = 0; r < cellMatrix.length; r++) {
+        for (var c = 0; c < cellMatrix[0].length; c++) {
+            if (cellMatrix[r][c] !== resultMat[r][c]) {
+                if (resultMat[r][c]) ctx.fillStyle = "#C2E7DA";
+                else ctx.fillStyle = "#330C2F";
+                ctx.ellipse(x + c * cellWidth + cellWidth / 2, y + r * cellWidth + cellWidth / 2, cellWidth / 2 - 1, cellWidth / 2 - 1);
+                ctx.fill();
             }
         }
     }
@@ -116,7 +118,12 @@ function mouseDownEvtHandler(evt) {
 function mouseMoveEvtHandler(evt) {
     if (!setting) return;
     var cellPos = getCellPos(getMousePos(evt));
+    cellMatrix[cellPos.r][cellPos.c] = recordedMatrix[cellPos.r][cellPos.c];
+    resultMat[cellPos.r][cellPos.c] = true;
+    draw();
     cellMatrix[cellPos.r][cellPos.c] = !recordedMatrix[cellPos.r][cellPos.c];
+    resultMat[cellPos.r][cellPos.c] = cellMatrix[cellPos.r][cellPos.c];
+    requestAnimationFrame(draw)
 }
 
 function getCellPos(mouse) {
@@ -127,21 +134,19 @@ function getCellPos(mouse) {
 }
 
 function step() {
-    var resultMat = cellMatrix.map(function (arr) {
+    resultMat = cellMatrix.map(function (arr) {
         return arr.slice();
     });
 
     for (var r = 0; r < cellMatrix.length; r++) {
         for (var c = 0; c < cellMatrix[0].length; c++) {
-            //console.log("iterating: " + r + " " + c);
             var numAlive = numCellsAlive(r, c);
             if (cellMatrix[r][c])
                 resultMat[r][c] = numAlive > 1 && numAlive < 4;
             else resultMat[r][c] = numAlive === 3;
         }
     }
-
-
+    draw();
     cellMatrix = resultMat;
 }
 
@@ -185,25 +190,13 @@ function removeIteratingThread() {
     }
 }
 
-function clearAll() {
+function clearAllThreads() {
+    if (typeof(iteratingThreadIds) === "undefined") return;
     while (iteratingThreadIds.length > 0) {
         clearInterval(iteratingThreadIds.splice(0, 1)[0]);
     }
 }
 
 function clearBoard() {
-    for (var r = 0; r < cellMatrix.length; r++) {
-        for (var c = 0; c < cellMatrix[0].length; c++) {
-            cellMatrix[r][c] = false;
-        }
-    }
-}
-
-function updateCellMatrix() {
-    var rows_ = document.getElementById("rows").value;
-    var cols_ = document.getElementById("cols").value;
-    rows = rows_;
-    cols = cols_;
-    initCellMatrix(rows, cols);
-    //requestAnimationFrame(draw);
+    renew();
 }
